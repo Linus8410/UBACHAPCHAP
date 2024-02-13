dba77a50c9915e71a7e80c80acb8753d664b528c
Linus8410 <99041785+Linus8410@users.noreply.github.com>
2024-02-13 13:16:24 -0800

    startactivity

--- app/src/main/java/com/example/ubachapchap/Splashs.java
+++ app/src/main/java/com/example/ubachapchap/Splashs.java
@@ -1,15 +1,22 @@
+
 package com.example.ubachapchap;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.icu.text.ListFormatter;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.net.Authenticator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,25 +25,83 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Action;
 
 public class Splashs extends AppCompatActivity {
-    private final static int LOGIN_REQUEST_CODE=7171;
+    private final static int LOGIN_REQUEST_CODE = 7171;
     private List<AuthUI.IdpConfig> providers;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener listener;
 
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(listener);
+    }
+
+    protected void onStop() {
+        if (firebaseAuth != null && listener != null)
+            firebaseAuth.removeAuthStateListener(listener);
+        super.onStop();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        delaySplashs();
-
+        setContentView(R.layout.signin);
+        init();
     }
+
+    private void init() {
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+        firebaseAuth = FirebaseAuth.getInstance();
+        listener = myFirebaseAuth -> {
+            FirebaseUser user = myFirebaseAuth.getCurrentUser();
+            if (user != null) {
+                delaySplashs();
+            } else {
+                showloginlayout();
+            }
+        };
+    }
+
+    private void showloginlayout() {
+        AuthMethodPickerLayout authMethodPickerLayout = new AuthMethodPickerLayout.Builder(R.layout.signin)
+                .setPhoneButtonId(R.id.phone)
+                .setGoogleButtonId(R.id.google)
+                .build();
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAuthMethodPickerLayout(authMethodPickerLayout)
+                        .setIsSmartLockEnabled(false)
+                        .setAvailableProviders(providers)
+                        .build(),
+                LOGIN_REQUEST_CODE
+        );
+    }
+
+
+
 
     private void delaySplashs() {
         Completable.timer(5, TimeUnit.SECONDS, AndroidSchedulers.mainThread()).subscribe(new Action() {
             @Override
             public void run() throws Exception {
-                Toast.makeText(Splashs.this, "Welcome to Ubachapchap", Toast.LENGTH_SHORT).show();
+                Toast.makeText(Splashs.this, "Welcome to Ubachapchap" + FirebaseAuth.getInstance().getCurrentUser().getUid(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOGIN_REQUEST_CODE) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            } else {
+                Toast.makeText(this, "[ERROR]" + response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
